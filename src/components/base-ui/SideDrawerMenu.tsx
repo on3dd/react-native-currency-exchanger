e95a97e5d4489451a1912b8f21bf6dce041606d6
on3dd <onedeadwave@gmail.com>
2020-09-30 20:30:33 +1000

    sidedrawer upd

--- src/components/base-ui/SideDrawerMenu.tsx
+++ src/components/base-ui/SideDrawerMenu.tsx
@@ -1,19 +1,43 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Pressable, View, SafeAreaView, ScrollView, Text, StyleSheet } from 'react-native';
 
 import { AppContext } from '@utils/AppContext';
-import { STATUSBAR_HEIGHT } from '@utils/constants';
+import { colors, STATUSBAR_HEIGHT } from '@utils/constants';
 import container from '@styles/container';
 
-const BaseSideDrawerMenu = () => {
+type BaseSideDrawerMenuOptionProps = {
+  name: string;
+}
+
+const BaseSideDrawerMenuOption: React.FC<BaseSideDrawerMenuOptionProps> = (
+  { name }: BaseSideDrawerMenuOptionProps,
+) => {
+  return (
+    <Pressable style={({ pressed }) => [{
+      backgroundColor: pressed
+        ? colors.cloud
+        : colors.white
+    }, styles.option]} onPress={() => console.log('bruh')}>
+      <Text style={styles.optionText}>{name}</Text>
+    </Pressable>
+  )
+}
+
+const BaseSideDrawerMenu: React.FC = () => {
   const context = useContext(AppContext);
 
   return (
     <View style={styles.view}>
       <View style={{ ...container, ...styles.menu }}>
         <Text style={{ ...context.style.primaryText, ...styles.text }}>
-          Nigger
-      </Text>
+          Currencies
+        </Text>
+        <SafeAreaView style={styles.scrollContainer}>
+          <ScrollView contentContainerStyle={styles.options}>
+            <BaseSideDrawerMenuOption name="USD" />
+            <BaseSideDrawerMenuOption name="RUB" />
+          </ScrollView>
+        </SafeAreaView>
       </View>
     </View>
   );
@@ -31,6 +55,23 @@ const styles = StyleSheet.create({
     fontSize: 30,
     fontWeight: "bold",
     marginBottom: 20,
+  },
+  scrollContainer: {
+    flexGrow: 1,
+    flexDirection: 'column',
+  },
+  options: {
+    flex: 1,
+  },
+  option: {
+    padding: 10,
+    marginBottom: 20,
+    justifyContent: 'center',
+    borderRadius: 5,
+    // backgroundColor: colors.white
+  },
+  optionText: {
+
   }
 });
 
